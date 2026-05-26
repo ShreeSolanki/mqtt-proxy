e6899b9eabdb5c6a25b3a19aca1535acd1211fb1
ShreeSolanki <90934136+ShreeSolanki@users.noreply.github.com>
2026-05-26 17:58:30 +0530

    Update proxy.py

--- proxy.py
+++ proxy.py
@@ -34,7 +34,7 @@ def on_message(client, userdata, msg):
             "Total_Power_W":         d.get("ptot",   0.0),
             "Energy_kWh":            d.get("kwhr",   0.0),
             "Timestamp":             d.get("ts",     0),
-            "Socket Allowed":        True
+            "Socket_Allowed":        True
         }
 
         print(f"Forwarding: {payload}")
